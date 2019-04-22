f81f49e9f7d61dbba857a85cd1702e0661592e1a
Nemanja Andric <aandric.nemanja@gmail.com>
2019-04-22 11:42:19 +0200

    asd

---
+++ js/jquery.js
@@ -0,0 +1,328 @@
+$(document).ready(function () {
+    $('.sidenav').sidenav({
+        closeOnClick: true,
+        draggable: true,
+    });
+
+    $('.materialboxed').materialbox();
+
+    $('.parallax').parallax();
+
+    $('.tabs').tabs({
+        swipeable: true,
+        duration: 50
+    });
+
+    $('.scrollspy').scrollSpy();
+
+    $('#usluga1').click(function () {
+
+        $('#usluga1-pr').show();
+        $('#usluga1').css("background-color", "#0E3655");
+        $('#usluga1').css("color", "#e2e2e2");
+        $('#usluga1.a').css("border-radius", "5px");
+
+        $('#usluga2-pr').hide();
+        $('#usluga2').css("background-color", "#e2e2e2");
+        $('#usluga2').css("color", "#0E3655");
+
+        $('#usluga3-pr').hide();
+        $('#usluga3').css("background-color", "#e2e2e2");
+        $('#usluga3').css("color", "#0E3655");
+
+        $('#usluga4-pr').hide();
+        $('#usluga4').css("background-color", "#e2e2e2");
+        $('#usluga4').css("color", "#0E3655");
+
+        $('#usluga5-pr').hide();
+        $('#usluga5').css("background-color", "#e2e2e2");
+        $('#usluga5').css("color", "#0E3655");
+
+        $('#usluga6-pr').hide();
+        $('#usluga6').css("background-color", "#e2e2e2");
+        $('#usluga6').css("color", "#0E3655");
+
+        $('#usluga7-pr').hide();
+        $('#usluga7').css("background-color", "#e2e2e2");
+        $('#usluga7').css("color", "#0E3655");
+
+        $('#usluga8-pr').hide();
+        $('#usluga8').css("background-color", "#e2e2e2");
+        $('#usluga8').css("color", "#0E3655");
+
+    });
+
+    $('#usluga2').click(function () {
+
+        $('#usluga2-pr').show();
+        $('#usluga2').css("background-color", "#0E3655");
+        $('#usluga2').css("color", "#e2e2e2");
+        $('#usluga2 > a').css("border-radius", "5px");
+
+        $('#usluga1-pr').hide();
+        $('#usluga1').css("background-color", "#e2e2e2");
+        $('#usluga1').css("color", "#0E3655");
+
+        $('#usluga3-pr').hide();
+        $('#usluga3').css("background-color", "#e2e2e2");
+        $('#usluga3').css("color", "#0E3655");
+
+        $('#usluga4-pr').hide();
+        $('#usluga4').css("background-color", "#e2e2e2");
+        $('#usluga4').css("color", "#0E3655");
+
+        $('#usluga5-pr').hide();
+        $('#usluga5').css("background-color", "#e2e2e2");
+        $('#usluga5').css("color", "#0E3655");
+
+        $('#usluga6-pr').hide();
+        $('#usluga6').css("background-color", "#e2e2e2");
+        $('#usluga6').css("color", "#0E3655");
+
+        $('#usluga7-pr').hide();
+        $('#usluga7').css("background-color", "#e2e2e2");
+        $('#usluga7').css("color", "#0E3655");
+
+        $('#usluga8-pr').hide();
+        $('#usluga8').css("background-color", "#e2e2e2");
+        $('#usluga8').css("color", "#0E3655");
+
+    });
+
+    $('#usluga3').click(function () {
+
+        $('#usluga3-pr').show();
+        $('#usluga3').css("background-color", "#0E3655");
+        $('#usluga3').css("color", "#e2e2e2");
+        $('#usluga3 > a').css("border-radius", "5px");
+
+        $('#usluga2-pr').hide();
+        $('#usluga2').css("background-color", "#e2e2e2");
+        $('#usluga2').css("color", "#0E3655");
+
+        $('#usluga1-pr').hide();
+        $('#usluga1').css("background-color", "#e2e2e2");
+        $('#usluga1').css("color", "#0E3655");
+
+        $('#usluga4-pr').hide();
+        $('#usluga4').css("background-color", "#e2e2e2");
+        $('#usluga4').css("color", "#0E3655");
+
+        $('#usluga5-pr').hide();
+        $('#usluga5').css("background-color", "#e2e2e2");
+        $('#usluga5').css("color", "#0E3655");
+
+        $('#usluga6-pr').hide();
+        $('#usluga6').css("background-color", "#e2e2e2");
+        $('#usluga6').css("color", "#0E3655");
+
+        $('#usluga7-pr').hide();
+        $('#usluga7').css("background-color", "#e2e2e2");
+        $('#usluga7').css("color", "#0E3655");
+
+        $('#usluga8-pr').hide();
+        $('#usluga8').css("background-color", "#e2e2e2");
+        $('#usluga8').css("color", "#0E3655");
+
+    });
+
+    $('#usluga4').click(function () {
+
+        $('#usluga4-pr').show();
+        $('#usluga4').css("background-color", "#0E3655");
+        $('#usluga4').css("color", "#e2e2e2");
+        $('#usluga4 > a').css("border-radius", "5px");
+
+        $('#usluga2-pr').hide();
+        $('#usluga2').css("background-color", "#e2e2e2");
+        $('#usluga2').css("color", "#0E3655");
+
+        $('#usluga3-pr').hide();
+        $('#usluga3').css("background-color", "#e2e2e2");
+        $('#usluga3').css("color", "#0E3655");
+
+        $('#usluga1-pr').hide();
+        $('#usluga1').css("background-color", "#e2e2e2");
+        $('#usluga1').css("color", "#0E3655");
+
+        $('#usluga5-pr').hide();
+        $('#usluga5').css("background-color", "#e2e2e2");
+        $('#usluga5').css("color", "#0E3655");
+
+        $('#usluga6-pr').hide();
+        $('#usluga6').css("background-color", "#e2e2e2");
+        $('#usluga6').css("color", "#0E3655");
+
+        $('#usluga7-pr').hide();
+        $('#usluga7').css("background-color", "#e2e2e2");
+        $('#usluga7').css("color", "#0E3655");
+
+        $('#usluga8-pr').hide();
+        $('#usluga8').css("background-color", "#e2e2e2");
+        $('#usluga8').css("color", "#0E3655");
+
+    });
+
+    $('#usluga5').click(function () {
+
+        $('#usluga5-pr').show();
+        $('#usluga5').css("background-color", "#0E3655");
+        $('#usluga5').css("color", "#e2e2e2");
+        $('#usluga5 > a').css("border-radius", "5px");
+
+        $('#usluga2-pr').hide();
+        $('#usluga2').css("background-color", "#e2e2e2");
+        $('#usluga2').css("color", "#0E3655");
+
+        $('#usluga3-pr').hide();
+        $('#usluga3').css("background-color", "#e2e2e2");
+        $('#usluga3').css("color", "#0E3655");
+
+        $('#usluga4-pr').hide();
+        $('#usluga4').css("background-color", "#e2e2e2");
+        $('#usluga4').css("color", "#0E3655");
+
+        $('#usluga1-pr').hide();
+        $('#usluga1').css("background-color", "#e2e2e2");
+        $('#usluga1').css("color", "#0E3655");
+
+        $('#usluga6-pr').hide();
+        $('#usluga6').css("background-color", "#e2e2e2");
+        $('#usluga6').css("color", "#0E3655");
+
+        $('#usluga7-pr').hide();
+        $('#usluga7').css("background-color", "#e2e2e2");
+        $('#usluga7').css("color", "#0E3655");
+
+        $('#usluga8-pr').hide();
+        $('#usluga8').css("background-color", "#e2e2e2");
+        $('#usluga8').css("color", "#0E3655");
+
+    });
+
+    $('#usluga6').click(function () {
+
+        $('#usluga6-pr').show();
+        $('#usluga6').css("background-color", "#0E3655");
+        $('#usluga6').css("color", "#e2e2e2");
+        $('#usluga6 > a').css("border-radius", "5px");
+
+        $('#usluga2-pr').hide();
+        $('#usluga2').css("background-color", "#e2e2e2");
+        $('#usluga2').css("color", "#0E3655");
+
+        $('#usluga3-pr').hide();
+        $('#usluga3').css("background-color", "#e2e2e2");
+        $('#usluga3').css("color", "#0E3655");
+
+        $('#usluga4-pr').hide();
+        $('#usluga4').css("background-color", "#e2e2e2");
+        $('#usluga4').css("color", "#0E3655");
+
+        $('#usluga5-pr').hide();
+        $('#usluga5').css("background-color", "#e2e2e2");
+        $('#usluga5').css("color", "#0E3655");
+
+        $('#usluga1-pr').hide();
+        $('#usluga1').css("background-color", "#e2e2e2");
+        $('#usluga1').css("color", "#0E3655");
+
+        $('#usluga7-pr').hide();
+        $('#usluga7').css("background-color", "#e2e2e2");
+        $('#usluga7').css("color", "#0E3655");
+
+        $('#usluga8-pr').hide();
+        $('#usluga8').css("background-color", "#e2e2e2");
+        $('#usluga8').css("color", "#0E3655");
+
+    });
+
+    $('#usluga7').click(function () {
+
+        $('#usluga7-pr').show();
+        $('#usluga7').css("background-color", "#0E3655");
+        $('#usluga7').css("color", "#e2e2e2");
+        $('#usluga7 > a').css("border-radius", "5px");
+
+        $('#usluga2-pr').hide();
+        $('#usluga2').css("background-color", "#e2e2e2");
+        $('#usluga2').css("color", "#0E3655");
+
+        $('#usluga3-pr').hide();
+        $('#usluga3').css("background-color", "#e2e2e2");
+        $('#usluga3').css("color", "#0E3655");
+
+        $('#usluga4-pr').hide();
+        $('#usluga4').css("background-color", "#e2e2e2");
+        $('#usluga4').css("color", "#0E3655");
+
+        $('#usluga5-pr').hide();
+        $('#usluga5').css("background-color", "#e2e2e2");
+        $('#usluga5').css("color", "#0E3655");
+
+        $('#usluga6-pr').hide();
+        $('#usluga6').css("background-color", "#e2e2e2");
+        $('#usluga6').css("color", "#0E3655");
+
+        $('#usluga1-pr').hide();
+        $('#usluga1').css("background-color", "#e2e2e2");
+        $('#usluga1').css("color", "#0E3655");
+
+        $('#usluga8-pr').hide();
+        $('#usluga8').css("background-color", "#e2e2e2");
+        $('#usluga8').css("color", "#0E3655");
+
+    });
+
+    $('#usluga8').click(function () {
+
+        $('#usluga8-pr').show();
+        $('#usluga8').css("background-color", "#0E3655");
+        $('#usluga8').css("color", "#e2e2e2");
+        $('#usluga8 > a').css("border-radius", "5px");
+
+        $('#usluga2-pr').hide();
+        $('#usluga2').css("background-color", "#e2e2e2");
+        $('#usluga2').css("color", "#0E3655");
+
+        $('#usluga3-pr').hide();
+        $('#usluga3').css("background-color", "#e2e2e2");
+        $('#usluga3').css("color", "#0E3655");
+
+        $('#usluga4-pr').hide();
+        $('#usluga4').css("background-color", "#e2e2e2");
+        $('#usluga4').css("color", "#0E3655");
+
+        $('#usluga5-pr').hide();
+        $('#usluga5').css("background-color", "#e2e2e2");
+        $('#usluga5').css("color", "#0E3655");
+
+        $('#usluga6-pr').hide();
+        $('#usluga6').css("background-color", "#e2e2e2");
+        $('#usluga6').css("color", "#0E3655");
+
+        $('#usluga7-pr').hide();
+        $('#usluga7').css("background-color", "#e2e2e2");
+        $('#usluga7').css("color", "#0E3655");
+
+        $('#usluga1-pr').hide();
+        $('#usluga1').css("background-color", "#e2e2e2");
+        $('#usluga1').css("color", "#0E3655");
+
+    });
+    // $('.fixed-action-btn').floatingActionButton();
+    // $('.datepicker').datepicker({
+    //     disableWeekends: true
+    // });
+    // $('.tooltiped').tooltip();
+});
+
+$(window).scroll(function () {
+    if ($("this").scrollTop() < 60) {
+        $('.bottomMenu').hide();
+    }
+    else {
+        $('.bottomMenu').show();
+    }
+
+});
